9a0e3fec527035851c3a580f7adc8825f1e5fb71
chnoguchi <chihiro3abc@gmail.com>
2020-12-31 19:31:46 +0900

    Update datasets/listdataset.py.

--- datasets/listdataset.py
+++ datasets/listdataset.py
@@ -26,8 +26,8 @@ class ListDataset(data.Dataset):
     def __getitem__(self, index):
         # img_path = self.img_path_list[index][:-1]
 
-        img_path = self.img_path_list[index]
-        label_path = self.label_path_list[index]
+        img_path = self.img_path_list[index][:-1]
+        label_path = self.label_path_list[index][:-1]
 
         print('img_path='.format(img_path))
         print('label_path='.format(label_path))
